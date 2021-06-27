32f66de839dd23d59adee44574cef68af4a635f6
okaforcj <okaforcj@icloud.com>
2021-06-27 16:12:59 +0100

    updated landedUser

--- landedUser.ts
+++ landedUser.ts
@@ -1,11 +1,21 @@
+import "stripe-v3"
+import ShippingDetailsAddress = stripe.ShippingDetailsAddress;
+
 type LandedUser = {
    firstName: string;
    lastName: string;
    email: string;
    uid: string;
+   addresses?: AddressType;
+   stripeId?: string;
    orderedProducts?: String[];
    wishlist?: string[]
    photoUrl?: string;
+}
+
+type AddressType = {
+   preferredAddress: ShippingDetailsAddress
+   otherAddresses: ShippingDetailsAddress
 }
 
 export default LandedUser;
