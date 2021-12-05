089414b9844563352d56b329d2fc4135406fe228
okaforcj <okaforcj@icloud.com>
2021-12-05 08:03:31 +0000

    init types

---
+++ IUser.ts
@@ -0,0 +1,22 @@
+type IUser = {
+   firstName: string;
+   lastName: string;
+   email: string;
+   uid: string;
+   dob?: string;
+   addresses?: AddressType[];
+   stripeId?: string;
+   orderedProducts?: String[];
+   wishlist?: string[]
+   photoUrl?: string;
+   createdAt: Date;
+   updatedAt: Date;
+}
+
+export type AddressType = {
+   address: any,
+   preferred: boolean
+   id: string;
+}
+
+export default IUser;
