1d85610b7f284d38c8bc7701d2bf3388e6153311
okaforcj <okaforcj@icloud.com>
2021-12-05 10:25:29 +0000

    update index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from "./types/IUser";
+export * from "./types/cartProductSchema";
+export * from "./types/orderSchema";
+export * from "./types/productReview";
+export * from "./types/productVariants";
+export * from "./types/reviewSchema";
