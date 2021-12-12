adb6e44eb9286edfd025b52515292f5d94a75163
okaforcj <okaforcj@icloud.com>
2021-12-12 16:57:50 +0000

    add user reg fields  😶‍🌫️

--- src/types/IUser.ts
+++ src/types/IUser.ts
@@ -1,20 +1,31 @@
+import { object, string, z } from "zod";
+
 export type IUser = {
-   firstName: string;
-   lastName: string;
-   email: string;
-   uid: string;
-   dob?: string;
-   addresses?: AddressType[];
-   stripeId?: string;
-   orderedProducts?: String[];
-   wishlist?: string[]
-   photoUrl?: string;
-   createdAt: Date;
-   updatedAt: Date;
-}
+  firstName: string;
+  lastName: string;
+  email: string;
+  uid: string;
+  dob?: string;
+  addresses?: AddressType[];
+  stripeId?: string;
+  orderedProducts?: String[];
+  wishlist?: string[];
+  photoUrl?: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export const UserRegSchema = object({
+  firstName: string(),
+  lastName: string(),
+  email: string(),
+  password: string()
+});
+
+export type UserRegDto= z.infer<typeof UserRegSchema>
 
 export type AddressType = {
-   address: any,
-   preferred: boolean
-   id: string;
-}
+  address: any;
+  preferred: boolean;
+  id: string;
+};
