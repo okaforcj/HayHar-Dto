fee298f23a1b05d1117680a3b2100e9d95a8130a
okaforcj <okaforcj@icloud.com>
2022-04-01 08:25:27 +0100

    add description again

--- CartProductSchema.ts
+++ CartProductSchema.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const CartProductSchema = z.object({
   name: z.string().nonempty(),
+  description: z.string(),
   price: z.number().gt(0),
   size: z.string(),
   color: z.string(),
