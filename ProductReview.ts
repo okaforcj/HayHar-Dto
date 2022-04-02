47cb8d4cafb9de64f477cbd47f04d3ccc0133ed1
okaforcj <okaforcj@icloud.com>
2022-04-02 14:39:02 +0100

    update reviews schemaa

--- ProductReview.ts
+++ ProductReview.ts
@@ -1,17 +1,19 @@
 import { z } from "zod";
 
+export const ReviewAggregation = z
+  .object({
+    1: z.number().default(0),
+    2: z.number().default(0),
+    3: z.number().default(0),
+    4: z.number().default(0),
+    5: z.number().default(0),
+  })
+  .default({});
 export const ProductAggSchema = z.object({
   noOfReviews: z.number().default(0),
   totalRating: z.number().default(0),
-  reviewAggregation: z
-    .object({
-      1: z.number().default(0),
-      2: z.number().default(0),
-      3: z.number().default(0),
-      4: z.number().default(0),
-      5: z.number().default(0),
-    })
-    .default({}),
+  reviewAggregation: ReviewAggregation,
 });
 
 export type ProductAgg = z.infer<typeof ProductAggSchema>;
+export type ReviewAgg = z.infer<typeof ReviewAggregation>;
